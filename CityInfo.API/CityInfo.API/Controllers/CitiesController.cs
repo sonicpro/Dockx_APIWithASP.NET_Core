d0b39e6d5ba08ea27a735da76aa9ec3ffd3e52ef
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-05-19 20:41:35 +0300

    Initial commit.

---
+++ CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Controllers
+{
+	[ApiController]
+	public class CitiesController : ControllerBase
+	{
+		[HttpGet("api/cities", Name = "CityInfoApi_CityList")]
+		public JsonResult GetCities()
+		{
+			return new JsonResult(new List<object>
+			{
+				new { id=1, Name="New York City" },
+				new { id=2, Name="Antwerp" }
+			});
+		}
+	}
+}
